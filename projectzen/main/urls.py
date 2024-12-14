d62e6ffeb74621291b03f5a12ac5912c6673af22
“NONAMEGG” <tyuutyavin@gmail.com>
2024-12-14 13:36:36 +0300

    authentication added

--- projectzen/main/urls.py
+++ projectzen/main/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('all_projects', views.all_projects, name='all_projects')
+    path('all_projects', views.all_projects, name='all_projects'),
+     path('api/login/', views.login, name='login'),
+    path('api/signup/', views.signup, name='signup'),
+    path('api/test-token/', views.test_token, name='test_token'),
+    path('login/', views.login_page, name='login_page'),
+    path('signup/', views.signup_page, name='signup_page'),
 ]
